ffc071fbe5489c3948928adfbddac90db4bf4661
aliibnemasud <mdmasudrony@gmail.com>
2022-05-26 23:58:50 +0600

    Question ans added

--- src/componants/pages/Blog/Blog.js
+++ src/componants/pages/Blog/Blog.js
@@ -4,6 +4,9 @@ const Blog = () => {
     return (
         <div>
             <h1 className='text-center'>Hello Blogs!</h1>
+            <h1 className="text-2xl">How will you improve the performance of a React Application?</h1>
+            <p>Memoizing React components to prevent unnecessary re-renders</p>
+            <h1 className="text-2xl">What are the different ways to manage a state in a React application?</h1>
         </div>
     );
 };
