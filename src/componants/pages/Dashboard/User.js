5cbe9ea62469f34009764f1805b066abfe8c44db
aliibnemasud <mdmasudrony@gmail.com>
2022-05-26 13:34:39 +0600

    Making admin done

--- src/componants/pages/Dashboard/User.js
+++ src/componants/pages/Dashboard/User.js
@@ -1,14 +1,22 @@
-import React from 'react';
+const User = ({ user, index }) => {
+    const { email, role } = user; 
 
-const User = ({user, index}) => {
-    const {email} = user;
+
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT'
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+
     return (
         <tr>
             <th>{index + 1}</th>
-            <td>Name</td>            
+            <td>Name</td>
             <td>{email}</td>
             <td> <button className='btn btn-sm btn-warning'>Role</button> </td>
-            <td> <button className='btn btn-sm btn-success'>Make Admin</button> </td>
+            <td> <button className='btn btn-sm btn-success' disabled={role == 'admin'} onClick={makeAdmin}>{role ? 'Admin': 'Make Admin'}</button> </td>
             <td> <label for="delete-order" class="btn btn-sm btn-error">Delete</label></td>
         </tr>
     );
